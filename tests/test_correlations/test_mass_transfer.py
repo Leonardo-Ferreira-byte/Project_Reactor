d378a0a96068255086352cf1af1286f6a52bf95d
Leonardo-Ferreira <leosilva764@hotmail.com>
2024-01-03 18:20:31 -0300

    fix: file test

--- tests/test_correlations/test_mass_transfer.py
+++ tests/test_correlations/test_mass_transfer.py
@@ -1,6 +1,8 @@
 from correlations.solubility import get_viscosity
 from correlations.mass_transfer import aS_fun, kiL_aL_fun, kiS_aS_fun
 from correlations.data import roW, get_volume_molar_oil
+from correlations.mass_transfer import get_diffusion_oil, get_diffusion_H2, get_diffusion_H2S
+from correlations.data import roW, vH2, vH2S, get_API, get_volume_molar_oil
 
 
 def test_diffusion_coefficients():
@@ -30,3 +32,20 @@ def test_diffusion_coefficients():
     assert abs(kiS_aS_fun(D1, viscosity, rho0, P, T, GL, aS) - 2.73e-2) < 1e-4
     assert abs(kiS_aS_fun(D2, viscosity, rho0, P, T, GL, aS) - 7.00e-2) < 1e-4
     assert abs(kiS_aS_fun(D4, viscosity, rho0, P, T, GL, aS) - 6.17e-2) < 1e-4
+
+
+def test_diffusion_coefficients():
+
+    T = 370 + 273.15
+    T_MeABP = (451 + 273.15)*1.8
+    Mm = 420
+    rho0 = 0.9146
+    d15_6 = rho0 / roW
+    API = get_API(d15_6)
+    viscosity = get_viscosity(T, API)
+    vL = get_volume_molar_oil(T_MeABP, d15_6, Mm)
+    assert abs(get_diffusion_oil(T, viscosity, vL) - 3.41e-5) < 1e-7
+    assert abs(get_diffusion_H2(T, viscosity, vL, vH2) - 1.49e-4) < 1e-6
+    assert abs(get_diffusion_H2S(T, viscosity, vL, vH2S) - 1.23e-4) < 1e-6
+
+test_diffusion_coefficients()
